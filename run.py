983cac86fd0d43008d45689e0d0be06f08d6358b
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-06-27 10:58:34 +0000

    Create a function for validating the input entered for customer full name

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import re
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -18,9 +19,32 @@ def full_name():
     """
     Takes the full name and details of the client and updates spreadsheet
     """
-    print("Please enter your full name")
+    while True: 
 
-    customer_name = input("Enter your full name here: \n")
-    print(f"Thank you, {customer_name}\n")
+        print("Please enter your full name")
+        print("For example: Tarik Khan \n")
+
+        customer_name = input("Enter your full name here: \n")
+        print(f"Thank you, {customer_name}\n")
+
+        if validate_full_name(customer_name):
+            print(f"{Fore.GREEN}Name entered is valid\n")
+            break
+
+    return full_name
+
+def validate_full_name(customer_name):
+    """
+    Validates the input entered as the customers full name
+    """
+    regex_name = re.compile(r'^(Mr\.|Mrs\.|Ms\.) ([a-z]+)( [a-z]+)*( [a-z]+)*$', re.IGNORECASE)
+
+    res = regex_name.search(customer_name)
+
+    if res:
+        print("Valid")
+    else: print("Invalid")
+
 
 full_name()
+
