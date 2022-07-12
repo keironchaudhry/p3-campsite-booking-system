cb24ba39ffaef65064fad98431d27c9984dc7645
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-07-12 08:23:24 +0000

    Add print statement to confirm closing of application

--- run.py
+++ run.py
@@ -120,6 +120,7 @@ def main():
                 print(contact_details())
 
             elif user_menu_choice == 4:
+                print(colored (("You are now exiting the program. Application now closing."), 'blue'))
                 sys.exit()
     
             elif user_menu_choice >= 5:
